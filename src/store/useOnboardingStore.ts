8259a868441dc8f6c4cf93627ce924c8ec7cf19c
JeonSuna <jsa020910@gmail.com>
2025-10-07 23:21:10 +0900

    feat: store 추가

---
+++ src/store/useOnboardingStore.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+
+//닉네임, 이메일 , 비번 , 전번 , 사용자 , 이메일 인증 여부
+interface OnboardingState {
+  nickname: string;
+  //   gender: "male" | "female" | null;
+  phone: string;
+  emailVerified: boolean;
+  loginId: string;
+  email: string;
+  memberType: string;
+  password: string;
+  setTemp: (field: Partial<OnboardingState>) => void;
+  reset: () => void;
+}
+
+export const useOnboardingState = create<OnboardingState>((set) => ({
+  nickname: "",
+  password: "",
+  //   gender: null,
+  phone: "",
+  emailVerified: false,
+  loginId: "",
+  email: "",
+  memberType: "",
+
+  setTemp: (field) => set((state) => ({ ...state, ...field })),
+  reset: () =>
+    set({
+      nickname: "",
+      //   gender: null,
+      //   birth: "",
+      password: "",
+      phone: "",
+      emailVerified: false,
+      email: "",
+      memberType: "",
+    }),
+}));
