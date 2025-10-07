9e7ab7cf4b348fabe012f4bba75de44b90341cfb
JeonSuna <jsa020910@gmail.com>
2025-10-07 21:13:02 +0900

    chore : tanstack-query, axios 설치

---
+++ src/api/apiMember.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+//멤버 api
+
+const api = axios.create({
+  baseURL:
+    "http://saksakfood-api-gateway-s-da7e9-110329723-31b4b99c070a.kr.lb.naverncp.com/member",
+});
+
+export default api;
