783e63abd9794fd4a45289f45f731d8a27c03b22
JeonSuna <jsa020910@gmail.com>
2025-10-10 01:24:07 +0900

    feat: icon import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite"; // 👈 추가
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: { "@": "/src", "@assets": "/src/assets" },
+  },
 });
