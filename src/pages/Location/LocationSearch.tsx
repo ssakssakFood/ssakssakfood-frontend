f047c07ee4209a99aa0fd5b571d7cea31b9da56c
JeonSuna <jsa020910@gmail.com>
2025-10-13 01:14:31 +0900

    faet: nav이동

--- src/pages/Location/LocationSearch.tsx
+++ src/pages/Location/LocationSearch.tsx
@@ -164,7 +164,7 @@ export default function LocationSearch() {
     try {
       if (mode === "call-api") {
         await postLocation(payload);
-        navigate(-1);
+        navigate("/location/edit");
       } else {
         navigate(-1);
       }
