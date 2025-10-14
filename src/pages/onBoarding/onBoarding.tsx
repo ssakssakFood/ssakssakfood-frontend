dfb903ac2fd748163cce86ddf548acdf329c6621
JeonSuna <jsa020910@gmail.com>
2025-10-14 21:26:35 +0900

    design:onboarding ui-1

--- src/pages/onBoarding/onBoarding.tsx
+++ src/pages/onBoarding/onBoarding.tsx
@@ -71,9 +71,11 @@ export default function OnBoardingPage() {
           </div>
         </section>
         {data && (
-          <p className="body-r-14 text-main2 p">
+          <p
+            className={`body-r-14 ${nicknameCheck ? "text-red" : "text-main2"} p`}
+          >
             {nicknameCheck
-              ? "이미 사용중인 닉네임이 있습니다."
+              ? "이미 사용중인 닉네임입니다."
               : "사용 가능한 닉네임입니다."}
           </p>
         )}
