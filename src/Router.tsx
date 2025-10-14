07595f79bacfe858ea46930f445a089eeda14200
JeonSuna <jsa020910@gmail.com>
2025-10-14 23:35:25 +0900

    feat: 아이디 비밀번호 page

--- src/Router.tsx
+++ src/Router.tsx
@@ -16,6 +16,7 @@ import PxLayout from "./layout/PxLayoyt";
 import CategoryPage from "./pages/Category/CategoryPage";
 import MenuDetailPage from "./pages/Menu/MenuDetailPage";
 import OnboardingNumber from "@/pages/onBoarding/onBoardingNumber";
+import OnBoardingCardPage from "@/pages/onBoarding/onBoardingCard";
 
 const router = createBrowserRouter([
   {
@@ -40,6 +41,7 @@ const router = createBrowserRouter([
       { path: "/onBoarding/confirm", element: <OnBoardingConfirmPage /> },
       { path: "/onBoarding/number", element: <OnboardingNumber /> },
       { path: "/onBoarding/pass", element: <OnBoardingPassPage /> },
+      { path: "/onBoarding/card", element: <OnBoardingCardPage /> },
       //위치수정
       { path: "/location/edit", element: <LocationEdit /> },
       { path: "/location/search", element: <LocationSearch /> },
