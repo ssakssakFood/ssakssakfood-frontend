7d9a22b9e79eeb39c7890b3a1a0277ea5c184081
ParkSumin <fhsjdvs@gmail.com>
2025-10-27 22:53:53 +0900

    style: Layout

--- src/layout/HomePage.tsx
+++ src/layout/HomePage.tsx
@@ -1,11 +1,14 @@
 // Layout.tsx
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { LocationHeader } from "../components/Headers";
 import SearchInput from "@/components/SearchInput";
 import useGeolocation from "@/hooks/useGeolocation";
+import { getMemberType } from "@/utils/getUserInfo";
 
 export default function Layout() {
   const { fullAdress, loading, error } = useGeolocation();
+  const location = useLocation();
+  const memberType = getMemberType();
 
   const displayLocation = loading
     ? "위치 정보를 불러오는 중..."
@@ -13,12 +16,16 @@ export default function Layout() {
       ? "위치 정보를 가져올 수 없습니다."
       : fullAdress || "위치를 확인할 수 없음";
 
+  const shouldShowHeader = !(memberType === "OWNER" && location.pathname === "/");
+
   return (
     <div className="w-full max-w-[401px] mx-auto overflow-x-hidden">
-      <div className="w-full sticky top-0 z-[1000] bg-white py-2">
-        <LocationHeader location={displayLocation} />
-        <SearchInput className="bg-[#F3F3F3] px-[16px] py-[10px] rounded-3xl mx-6" />
-      </div>
+      {shouldShowHeader && (
+        <div className="w-full sticky top-0 z-[1000] bg-white py-2">
+          <LocationHeader location={displayLocation} />
+          <SearchInput className="bg-[#F3F3F3] px-[16px] py-[10px] rounded-3xl mx-6" />
+        </div>
+      )}
       <main className="px-6">
         <Outlet />
       </main>
