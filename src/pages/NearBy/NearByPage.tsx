e52a783008886b59fbabc81f975e7640a72cf75c
JeonSuna <jsa020910@gmail.com>
2025-10-28 16:44:06 +0900

    feat: 대표 위치 변경시 session에 저장

--- src/pages/NearBy/NearByPage.tsx
+++ src/pages/NearBy/NearByPage.tsx
@@ -22,12 +22,13 @@ declare global {
 }
 export default function NearbyPage() {
   const { latitude, longitude, loading, error } = useGeolocation();
+  console.log(latitude, longitude, "위치치치치");
   const getNearbyAlong = useAlongRoute();
 
   const [ismodal, setIsModal] = useState(false);
   // const [storeModal, setStoreModal] = useState(false);
   const [selectedMarker, setSelectedMarker] = useState<number | undefined>(
-    undefined,
+    undefined
   );
 
   const mapRef = useRef<HTMLDivElement>(null);
@@ -41,7 +42,7 @@ export default function NearbyPage() {
   };
 
   const [selectedRoute, setSelectedRoute] = useState<NearbyResponseDto | null>(
-    null,
+    null
   );
 
   //출발지,목적지 상태
@@ -59,7 +60,7 @@ export default function NearbyPage() {
       lng: number;
       storeName?: string;
       distanceMeters?: number;
-    }>,
+    }>
   ) => {
     const kakao = window.kakao;
     if (!mapInstanceRef.current || !kakao) return;
@@ -130,7 +131,7 @@ export default function NearbyPage() {
             console.log(res, "근처경로조회");
             renderStoreMarkers(res?.markers ?? []);
           },
-        },
+        }
       );
     }); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading, error, latitude, longitude]);
@@ -164,7 +165,7 @@ export default function NearbyPage() {
           console.log("근처경로조회,루트임");
           renderStoreMarkers(res?.markers ?? []);
         },
-      },
+      }
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
